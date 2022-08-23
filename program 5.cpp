ba08b3d7bf4d27c397f5899526cae19bfefbdb44
tina <102860264+tinachelwanii@users.noreply.github.com>
2022-08-24 02:41:11 +0530

    Add files via upload

---
+++ program 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namspace std;
+
+int main()
+{
+    char grade = 'A';
+    string phrase = "Giraffe Academy";
+    int age = 50;
+    double gpa = 3.3;
+    bool isMale = true;
+    return 0;
+}
---
+++ program2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" <<endl;
+    cout << "Tina" << endl;
+
+    return 0;
+}
